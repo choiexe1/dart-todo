7062812afdc8b1ed18b6498d19a9601bc1f8c31c
Junsung Choi <choiexe1@gmail.com>
2025-04-03 01:09:41 +0900

    test: TodoRepository Test

---
+++ test/repository/todo_repository_impl_test.dart
@@ -0,0 +1,25 @@
+import 'package:dart_todo/data_source/json_file_data_source.dart';
+import 'package:dart_todo/dto/todo_dto.dart';
+import 'package:dart_todo/model/todo.dart';
+import 'package:dart_todo/repository/todo_repository.dart';
+import 'package:dart_todo/repository/todo_repository_impl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const TodoRepository repository = TodoFileRepositoryImpl(
+    JsonFileDataSource<Todo>('data/backup.dat'),
+  );
+
+  final TodoDTO dto = TodoDTO(
+    title: 'hi',
+    userId: 1,
+    createdAt: DateTime.parse('2025-04-03 01:00'),
+  );
+
+  test('create()', () async {
+    Todo created = await repository.create(dto);
+    Todo? todo = await repository.findOne(created.id);
+
+    expect(todo, isNotNull);
+  });
+}
